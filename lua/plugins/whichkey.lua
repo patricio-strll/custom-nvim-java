4f38dba04f1b4c5bd1b3dfc6134fbac16b2290e5
Piter N <gabrielliendo1257@gmail.com>
2024-07-27 09:58:02 -0500

    Key Configuration

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -6,18 +6,18 @@ return {
         local which_key = require('which-key')
 
         -- call the setup function with default properties
-        which_key.setup()
+        -- which_key.setup()
 
         -- Register prefixes for the different key mappings we have setup previously
-        which_key.register({
-            ['<leader>/'] = {name = "Comments", _ = 'which_key_ignore'},
-            ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-            ['<leader>d'] = {name = '[D]ebug' , _ = 'which_key_ignore' },
-            ['<leader>e'] = {name = '[E]xplorer', _ = 'which_key_ignore'},
-            ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-            ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-            ['<leader>J'] = { name = '[J]ava', _ = 'which_key_ignore' },
-            ['<leader>w'] = {name = '[W]indow', _ = 'which_key_ignore'}
+        which_key.add({
+            {'<leader>/', desc = "Comments"},
+            {'<leader>c', desc = '[C]ode'},
+            {'<leader>d', desc = '[D]ebug'},
+            {'<leader>e', desc = '[E]xplorer'},
+            {'<leader>f', desc = '[F]ind'},
+            {'<leader>g', desc = '[G]it'},
+            {'<leader>J', desc = '[J]ava'},
+            {'<leader>w', desc = '[W]indow'}
         })
     end
 }
