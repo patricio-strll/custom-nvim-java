083cc2abd523f7d97f32938734f4a4eb43d228a5
Piter N <gabrielliendo1257@gmail.com>
2024-07-27 11:34:11 -0500

    remodeled lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -39,33 +39,32 @@ return {
             -- get access to the lspconfig plugins functions
             local lspconfig = require("lspconfig")
 
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
             -- setup the lua language server
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
+            -- lspconfig.lua_ls.setup({
+            --     capabilities = capabilities,
+            -- })
 
             -- setup the typescript language server
-            lspconfig.tsserver.setup({
-                capabilities = capabilities,
-            })
+            -- lspconfig.tsserver.setup({
+            --     capabilities = capabilities,
+            -- })
 
             -- Set vim motion for <Space> + c + h to show code documentation about the code the cursor is currently over if available
-            vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
-            -- Set vim motion for <Space> + c + d to go where the code/variable under the cursor was defined
-            vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
-            -- Set vim motion for <Space> + c + a for display code action suggestions for code diagnostics in both normal and visual mode
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
-            -- Set vim motion for <Space> + c + r to display references to the code under the cursor
-            vim.keymap.set("n", "<leader>cr", require("telescope.builtin").lsp_references, { desc = "[C]ode Goto [R]eferences" })
-            -- Set vim motion for <Space> + c + i to display implementations to the code under the cursor
-            vim.keymap.set("n", "<leader>ci", require("telescope.builtin").lsp_implementations, { desc = "[C]ode Goto [I]mplementations" })
-            -- Set a vim motion for <Space> + c + <Shift>R to smartly rename the code under the cursor
-            vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
-            -- Set a vim motion for <Space> + c + <Shift>D to go to where the code/object was declared in the project (class file)
-            vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
-            vim.keymap.set('n', '<leader>cf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>')
+            -- vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
+            -- -- Set vim motion for <Space> + c + d to go where the code/variable under the cursor was defined
+            -- vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
+            -- -- Set vim motion for <Space> + c + a for display code action suggestions for code diagnostics in both normal and visual mode
+            -- vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
+            -- -- Set vim motion for <Space> + c + r to display references to the code under the cursor
+            -- vim.keymap.set("n", "<leader>cr", require("telescope.builtin").lsp_references, { desc = "[C]ode Goto [R]eferences" })
+            -- -- Set vim motion for <Space> + c + i to display implementations to the code under the cursor
+            -- vim.keymap.set("n", "<leader>ci", require("telescope.builtin").lsp_implementations, { desc = "[C]ode Goto [I]mplementations" })
+            -- -- Set a vim motion for <Space> + c + <Shift>R to smartly rename the code under the cursor
+            -- vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
+            -- -- Set a vim motion for <Space> + c + <Shift>D to go to where the code/object was declared in the project (class file)
+            -- vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
+            -- vim.keymap.set('n', '<leader>cf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>')
         end
     }
 }
